03703452ca6700c57815683ae3c359aa80eab52c
Robert-Andrei OPRESCU <robert.oprescu@stud.aero.upb.ro>
2021-11-23 09:00:22 +0200

    added styling to cards and props

--- netbet-feed-app/src/Card.js
+++ netbet-feed-app/src/Card.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import './Card.css';
+import vinyl from './assets/vinyl-record.svg';
 
 
 
-const Card = ({title, artist, label, year}) => {
+const Card = (props) => {
+    // console.log(props)
     return(
-        <div className='bg-light-blue dib br3 pa3 ma2 grow'>
-            <h2>Title: {title}</h2>
-            <h3>Artist: {artist}</h3>
-            <h3>Label: {label}</h3>
-            <h3>Year: {year}</h3>
+        <div className='bg-light-blue dib br3 pa3 ma2 grow const-size'>
+            <img className='vinyl' src={vinyl} alt='Record'/>
+            <h2>Title: {props.albums.title}</h2>
+            <h3>Artist: {props.albums.artist}</h3>
+            <h3>Label: {props.albums.label}</h3>
+            <h3>Year: {props.albums.year}</h3>
         </div>
     );
 }
